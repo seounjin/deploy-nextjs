2e444dc78b4baa904d8bf41f0b7c09703ddf1923
Invanda7 <143367233+Invanda7@users.noreply.github.com>
2024-04-15 17:47:18 +0900

    Update : 차크라 ui 설치

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import { Button } from "@chakra-ui/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Git Action 연습입니다. h1</h1>
-      <h2>Git Action 연습 h2</h2>
+      <h2>Git Action 연습입니다. h2</h2>
       <h3>Git Action 연습 h3</h3>
       <h4>Git Action 연습 h4</h4>
+      <Button>차크라</Button>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
