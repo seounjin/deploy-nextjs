00156c67474bc397ab8cbff3ef7ff5121ed68aa6
Invanda7 <143367233+Invanda7@users.noreply.github.com>
2024-04-15 17:40:50 +0900

    Update : h1태그 수정

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Git Action 연습 h1</h1>
+      <h1>Git Action 연습입니다. h1</h1>
       <h2>Git Action 연습 h2</h2>
       <h3>Git Action 연습 h3</h3>
       <h4>Git Action 연습 h4</h4>
